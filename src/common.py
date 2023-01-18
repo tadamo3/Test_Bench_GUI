89d93c33ff52c8ff1766a8006031a85c471a9265
tadamo3 <truizw@gmail.com>
2023-01-17 22:22:49 -0500

    Added second page for programs with return to home button

--- src/common.py
+++ src/common.py
@@ -9,6 +9,9 @@
 import tkinter
 import customtkinter
 
+import home_page
+import programs_page
+
 # Functions
 def set_appearance(appearance_mode, default_color_theme):
     """! Sets the appearance of a given page
@@ -17,3 +20,40 @@ def set_appearance(appearance_mode, default_color_theme):
     """
     customtkinter.set_appearance_mode(appearance_mode)
     customtkinter.set_default_color_theme(default_color_theme)
+
+def generate_window(window_type, window_name):
+    """! Generates a given window type
+    @param window_type  Window type to be created
+    @param window_name  Name to be associated with created winow
+    @return window  The created window    
+    """
+    set_appearance("Dark", "blue")
+
+    # Generate correct window type
+    if (window_type == 'Home'):
+        window = home_page.HomePage(window_name)
+
+    elif (window_type == 'Programs'):
+        window = programs_page.ProgramsPage(window_name)
+    
+    else:
+        print('Not a valid window type')
+    
+    if (window != None):
+        window.state('zoomed')
+
+        return window
+    else:
+        print('Error in creating window')
+
+def start_window(window_to_start):
+    """! Start the main loop of a given window
+    @param window_to_start  The window to call mainloop() function on
+    """
+    window_to_start.mainloop()
+
+def close_window(window_to_close):
+    """! Closes a given window
+    @param window_to_close  The window to call the destroy() function on
+    """
+    window_to_close.destroy()
--- src/home_page.py
+++ src/home_page.py
@@ -12,20 +12,35 @@ import customtkinter
 import tkinter.messagebox
 
 import common
+import programs_page
 
 # Global constants
-## The width of the home page
-WIDTH = 780
+## The width and height of the home page
+WIDTH_HOME_PAGE = 780
+HEIGHT_HOME_PAGE = 520
 
-## The height of the home page
-HEIGHT = 520
+## Relative positions and appearance of the button to switch to the programs page
+RELX_BTN_PROGRAMS_PAGE = 0.25
+RELY_BTN_PROGRAMS_PAGE = 0.25
+WIDTH_BTN_PROGRAMS_PAGE = 500
+HEIGHT_BTN_PROGRAMS_PAGE = 100
+BORDER_WIDTH_BTN_PROGRAMS_PAGE = 0
+CORNER_RADIUS_BTN_PROGRAMS_PAGE = 8
 
 # Functions
-def button_event(text):
-    """! Prints out a given text
-    @param textbox_name_of_program  The text to be printed out
+def setup_home_page():
+    """! Setups and starts the home page window with an appropriate name
     """
-    print(text)
+    window_home = common.generate_window('Home', 'Home - Zimmer Biomet Test Bench GUI')
+    common.start_window(window_home)
+
+def btn_event_switch_to_programs_page(current_window):
+    """! Upon calling, destroys the home page and creates the programs page
+    @param current_window   Window to be closed
+    """
+    common.close_window(current_window)
+
+    programs_page.setup_programs_page()
 
 # Classes
 class HomePage(customtkinter.CTk):
@@ -44,19 +59,24 @@ class HomePage(customtkinter.CTk):
         self.title(name_of_page)
 
         ## Shape of the home page
-        self.geometry(f"{WIDTH} x {HEIGHT}")
+        self.geometry(f"{WIDTH_HOME_PAGE} x {HEIGHT_HOME_PAGE}")
 
         ## Closing procedure for home page window
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         ## Button to access the available test programs
-        btn_programs_page = customtkinter.CTkButton( text = "Programs",
-                                                        command = lambda: button_event("txtbx_name_of_program"),
-                                                        width = 120,
-                                                        height = 32,
-                                                        border_width = 0,
-                                                        corner_radius = 8)
-        btn_programs_page.place(relx = 0.3, rely = 0.3, anchor = tkinter.CENTER)
+        btn_programs_page = customtkinter.CTkButton(text            = "Programs",
+                                                    command         = lambda: btn_event_switch_to_programs_page(self),
+                                                    width           = WIDTH_BTN_PROGRAMS_PAGE,
+                                                    height          = HEIGHT_BTN_PROGRAMS_PAGE,
+                                                    border_width    = BORDER_WIDTH_BTN_PROGRAMS_PAGE,
+                                                    corner_radius   = CORNER_RADIUS_BTN_PROGRAMS_PAGE
+                                                    )
+
+        btn_programs_page.place(relx = RELX_BTN_PROGRAMS_PAGE, 
+                                rely = RELY_BTN_PROGRAMS_PAGE,
+                                anchor = tkinter.CENTER
+                                )
 
     def on_closing(self):
         """! Procedure to follow upon closing a window object
@@ -66,6 +86,4 @@ class HomePage(customtkinter.CTk):
 if __name__ == "__main__":
     """! Main program entry
     """
-    common.set_appearance("Dark", "blue")
-    window_home = HomePage("Home - Zimmer Biomet Test Bench")
-    window_home.mainloop()
+    setup_home_page()
